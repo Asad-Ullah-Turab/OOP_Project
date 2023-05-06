14d08e82440f19a9433ac812d0ff19c06ba3ac58
AsadUllah <Asad-Ullah-Turab@users.noreply.github.com>
2023-05-06 15:41:09 +0500

    Added and Linked SFML library

--- OOP_Project.cpp
+++ OOP_Project.cpp
@@ -1,15 +1,37 @@
 #include <iostream>
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+using namespace sf;
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    RenderWindow window(VideoMode(1366, 768), "OOP Project", Style::Default);
+
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+
+        // Update
+
+
+        // Draw
+        window.clear();
+        // Draw Stuff
+
+        // Finished drawing
+        window.display();
+    }
+    
+    cout << "Hello World!\n";
+    return 0;
 }
-
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
